ba2de527a24fd078c5759b3014c96bf21a21c46d
federicoalagna <federicoalagna01@gmail.com>
2024-05-17 08:17:27 +0200

    array con le bici

---
+++ snack1/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// creo un array con i nomi delle bici
+const biciDaCorsa = [ { nome: 'Bianchi', peso: 8 }, { nome: 'Trek', peso: 7 }, { nome: 'Cannondale', peso: 6 }, { nome: 'Specialized', peso: 9 } ];
+
+let biciLeggera = biciDaCorsa[0];
