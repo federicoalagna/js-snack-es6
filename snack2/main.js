180857c9328badc6ebc1a8b4ad24b1143b6befb4
federicoalagna <federicoalagna01@gmail.com>
2024-05-17 08:34:07 +0200

    array con le squadre di calcio

---
+++ snack2/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// array con le squadre di calcio
+const squadreCalcio = [
+    { nome: "Inter", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Juve", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Atalanta", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Lazio", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Fiorentina", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Verona", puntiTotali: 0, falliSubiti: 0 },
+    { nome: "Sassuolo", puntiTotali: 0, falliSubiti: 0 }
+  ];
